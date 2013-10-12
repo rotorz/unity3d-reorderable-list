3239e3d862f38bc32af9bd2ce782515c64ad4642
Lea Hayes <lea@rotorz.com>
2013-10-12 11:26:17 +0100

    Added `#pragma strict` to demonstration scripts and adjusted use of whitespace around import statements.

--- Demo/DemoBehaviourUnityScript.js
+++ Demo/DemoBehaviourUnityScript.js
@@ -1,7 +1,6 @@
 // Copyright (c) 2012-2013 Rotorz Limited. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
-
 #pragma strict
 
 import System.Collections.Generic;
--- Demo/Editor/DemoBehaviourUnityScriptEditor.js
+++ Demo/Editor/DemoBehaviourUnityScriptEditor.js
@@ -1,6 +1,7 @@
 // Copyright (c) 2012-2013 Rotorz Limited. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
+#pragma strict
 
 import Rotorz.ReorderableList;
 
