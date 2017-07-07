6a996ebf087664942f6f5d26ceac94719c52341e
Lea Hayes <lea@rotorz.com>
2017-07-07 21:14:34 +0100

    Simplify 'element adder menu' mechanism by utilizing `EditorMenu`.

--- assets/Editor/Collections/ElementAdderMenu/IElementAdderMenuCommand.cs
+++ assets/Editor/Collections/ElementAdderMenu/IElementAdderMenuCommand.cs
@@ -1,23 +1,22 @@
 // Copyright (c) Rotorz Limited. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root.
 
-using UnityEngine;
 
 namespace Rotorz.Games.Collections
 {
     /// <summary>
     /// Interface for a menu command that can be included in an <see cref="IElementAdderMenu"/>
     /// either by annotating an implementation of the <see cref="IElementAdderMenuCommand{TContext}"/>
-    /// interface with <see cref="ElementAdderMenuCommandAttribute"/> or directly by
-    /// calling <see cref="IElementAdderMenuBuilder{TContext}.AddCustomCommand"/>.
+    /// interface with <see cref="ElementAdderMenuCommandAttribute"/>.
     /// </summary>
     /// <typeparam name="TContext">Type of the context object that elements can be added to.</typeparam>
+    /// <seealso cref="ElementAdderMenuPopulator{TContext}"/>
     public interface IElementAdderMenuCommand<TContext>
     {
         /// <summary>
-        /// Gets the content of the menu command.
+        /// Gets the full path of the menu command.
         /// </summary>
-        GUIContent Content { get; }
+        string FullPath { get; }
 
         /// <summary>
         /// Determines whether the command can be executed.
